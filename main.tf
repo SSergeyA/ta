e0fcd8d14f491b590331243b12d33bc86c2632a5
Sergey Sokolov <5-5-5-5@mail.ru>
2022-07-21 10:29:56 +0300

    1

---
+++ main.tf
@@ -0,0 +1,64 @@
+
+provider "yandex" {
+  cloud_id  = "b1gd88frf016p6u7jbkb$"
+  folder_id = "b1gump4iofj1llimbreg"
+  zone      = "ru-central1-a"
+}
+
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8fte6bebi857ortlja"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("/home/sergey/.ssh/id_rsa")}"
+  }
+}
+
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+
+
+
+output "zone_id" {
+  value = yandex_compute_instance.vm-1.zone   
+}
+
+
+
+
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
